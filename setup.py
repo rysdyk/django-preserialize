61855163b8dc18e9c41fce5e6d8fa2e4e38ba258
srysdyk <srysdyk@iu.edu>
2019-12-13 11:12:58 -0500

    update requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ kwargs = {
     'packages': ['preserialize'],
     'include_package_data': True,
     'install_requires': [
-        'django>=1.8,<=1.11.16',
+        'django>=1.8,<=1.12',
     ],
     'test_suite': 'test_suite',
     'name': 'django-preserialize',
@@ -23,6 +23,7 @@ kwargs = {
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
     ],
 }
 
